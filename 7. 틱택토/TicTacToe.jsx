d9ab93639d935d8edb0e32fd146fff4d4fbcee60
Bersk3r <kiscwook@gmail.com>
2023-04-22 02:56:02 +0900

    chap 7-2 fin

--- 7. 틱택토/TicTacToe.jsx
+++ 7. 틱택토/TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useReducer} from 'react';
+import React, {useState, useReducer, useCallback} from 'react';
 import Table from "./Table";
 
 const initialState = {
@@ -7,19 +7,29 @@ const initialState = {
   tableData:[['','',''],['','',''],['','','']],
 };
 
+const SET_WINNER = 'SET_WINNER';
 const reducer = (state, action) => {
-  // state를 변경할 내용을 작성
+  switch(action.type) {
+    case SET_WINNER:
+      // state.winner = action.winner처럼 직접 변경하면 안 된다.
+      return {
+        ...state, // 스프레드 문법으로 기존 state가 얕은 복사가 된다. (불변성)
+        winner: action.winner,
+      };
+  }
 };
 const TicTacToe = () => {
-  const [state,dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
   // const [winner, setWinner] = useState('');
   // const [turn, setTurn] =  useState('O');
   // const [tableData, setTableData] = useState([['','',''],['','',''],['','','']]);
-
+  const onClickTable = useCallback(() => {
+      dispatch({ type: SET_WINNER, winner: 'O' });
+  },[]);
   return (
     <>
-      <Table></Table>
-      {winner && <div>{winner}님의 승리</div>}
+      <Table onClick={onClickTable} tableData={state.tableData}></Table>
+      {state.winner && <div>{state.winner}님의 승리</div>}
     </>
   );
 };
