1d9aa78368dba7f2ffc346ef646f53fba6256473
Bersk3r <kiscwook@gmail.com>
2023-04-24 02:05:52 +0900

    chap 7-3 fin

--- 7. 틱택토/TicTacToe.jsx
+++ 7. 틱택토/TicTacToe.jsx
@@ -7,7 +7,10 @@ const initialState = {
   tableData:[['','',''],['','',''],['','','']],
 };
 
-const SET_WINNER = 'SET_WINNER';
+export const SET_WINNER = 'SET_WINNER';
+export const CLICK_CELL = 'CLICK_CELL';
+export const CHANGE_TURN = 'CHANGE_TURN';
+
 const reducer = (state, action) => {
   switch(action.type) {
     case SET_WINNER:
@@ -16,6 +19,23 @@ const reducer = (state, action) => {
         ...state, // 스프레드 문법으로 기존 state가 얕은 복사가 된다. (불변성)
         winner: action.winner,
       };
+    case CLICK_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...tableData[action.row]]; // immer라는 라이브러리로 가독성 문제 해결이 가능하다.
+      tableData[action.row][action.cell] = state.turn;
+      return {
+        ...state,
+        tableData,
+      }
+    }
+
+    case CHANGE_TURN:
+    {
+      return {
+        ...state,
+        turn: state.turn === 'O' ? 'X' : 'O',
+      };
+    }
   }
 };
 const TicTacToe = () => {
@@ -28,7 +48,7 @@ const TicTacToe = () => {
   },[]);
   return (
     <>
-      <Table onClick={onClickTable} tableData={state.tableData}></Table>
+      <Table onClick={onClickTable} tableData={state.tableData} dispatch={dispatch}></Table>
       {state.winner && <div>{state.winner}님의 승리</div>}
     </>
   );
