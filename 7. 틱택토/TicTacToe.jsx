d3390ac537a7f053d179140a2b97ce84502fa5ab
Bersk3r <kiscwook@gmail.com>
2023-04-27 02:53:23 +0900

    chap 7-4 fin

--- 7. 틱택토/TicTacToe.jsx
+++ 7. 틱택토/TicTacToe.jsx
@@ -1,57 +1,120 @@
-import React, {useState, useReducer, useCallback} from 'react';
-import Table from "./Table";
+import React, { useEffect, useReducer, useCallback } from 'react';
+import Table from './Table';
 
 const initialState = {
   winner: '',
   turn: 'O',
-  tableData:[['','',''],['','',''],['','','']],
+  tableData: [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ],
+  recentCell: [-1, -1],
 };
 
 export const SET_WINNER = 'SET_WINNER';
 export const CLICK_CELL = 'CLICK_CELL';
 export const CHANGE_TURN = 'CHANGE_TURN';
+export const RESET_GAME = 'RESET_GAME';
 
 const reducer = (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case SET_WINNER:
-      // state.winner = action.winner처럼 직접 변경하면 안 된다.
+      // state.winner = action.winner; 이렇게 하면 안됨.
       return {
-        ...state, // 스프레드 문법으로 기존 state가 얕은 복사가 된다. (불변성)
+        ...state,
         winner: action.winner,
       };
     case CLICK_CELL: {
       const tableData = [...state.tableData];
-      tableData[action.row] = [...tableData[action.row]]; // immer라는 라이브러리로 가독성 문제 해결이 가능하다.
+      tableData[action.row] = [...tableData[action.row]]; // immer라는 라이브러리로 가독성 해결
       tableData[action.row][action.cell] = state.turn;
       return {
         ...state,
         tableData,
-      }
+        recentCell: [action.row, action.cell],
+      };
     }
-
-    case CHANGE_TURN:
-    {
+    case CHANGE_TURN: {
       return {
         ...state,
         turn: state.turn === 'O' ? 'X' : 'O',
       };
     }
+    case RESET_GAME: {
+      return {
+        ...state,
+        turn: 'O',
+        tableData: [
+          ['', '', ''],
+          ['', '', ''],
+          ['', '', ''],
+        ],
+        recentCell: [-1, -1],
+      };
+    }
+    default:
+      return state;
   }
 };
+
 const TicTacToe = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const { tableData, turn, winner, recentCell } = state;
   // const [winner, setWinner] = useState('');
-  // const [turn, setTurn] =  useState('O');
-  // const [tableData, setTableData] = useState([['','',''],['','',''],['','','']]);
+  // const [turn, setTurn] = useState('O');
+  // const [tableData, setTableData] = useState([['', '', ''], ['', '', ''], ['', '', '']]);
+
   const onClickTable = useCallback(() => {
-      dispatch({ type: SET_WINNER, winner: 'O' });
-  },[]);
+    dispatch({ type: SET_WINNER, winner: 'O' });
+  }, []);
+
+  useEffect(() => {
+    const [row, cell] = recentCell;
+    if (row < 0) {
+      return;
+    }
+    let win = false;
+    if (tableData[row][0] === turn && tableData[row][1] === turn && tableData[row][2] === turn) {
+      win = true;
+    }
+    if (tableData[0][cell] === turn && tableData[1][cell] === turn && tableData[2][cell] === turn) {
+      win = true;
+    }
+    if (tableData[0][0] === turn && tableData[1][1] === turn && tableData[2][2] === turn) {
+      win = true;
+    }
+    if (tableData[0][2] === turn && tableData[1][1] === turn && tableData[2][0] === turn) {
+      win = true;
+    }
+    console.log(win, row, cell, tableData, turn);
+    if (win) { // 승리시
+      dispatch({ type: SET_WINNER, winner: turn });
+      dispatch({ type: RESET_GAME });
+    } else {
+      let all = true; // all이 true면 무승부라는 뜻
+      tableData.forEach((row) => { // 무승부 검사
+        row.forEach((cell) => {
+          if (!cell) {
+            all = false;
+          }
+        });
+      });
+      if (all) {
+        dispatch({ type: SET_WINNER, winner: null });
+        dispatch({ type: RESET_GAME });
+      } else {
+        dispatch({ type: CHANGE_TURN });
+      }
+    }
+  }, [recentCell]);
+
   return (
-    <>
-      <Table onClick={onClickTable} tableData={state.tableData} dispatch={dispatch}></Table>
-      {state.winner && <div>{state.winner}님의 승리</div>}
-    </>
-  );
+      <>
+        <Table onClick={onClickTable} tableData={tableData} dispatch={dispatch} />
+        {winner && <div>{winner}님의 승리</div>}
+      </>
+  )
 };
 
 export default TicTacToe;
