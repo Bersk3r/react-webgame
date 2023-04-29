7f03bc718c69c41e0d7bedb6d4071bdc6f730d04
Bersk3r <kiscwook@gmail.com>
2023-04-30 00:00:22 +0900

    chap 8-2 fin

---
+++ 8. 지뢰찾기/MineSearch.jsx
@@ -0,0 +1,55 @@
+import React, {useReducer, createContext, useMemo} from 'react';
+import Table from './Table';
+import Form from './Form';
+
+export const CODE = {
+  MINE: -7,
+  NORMAL: -1,
+  QUESTION: -2,
+  FLAG: -3,
+  QUESTION_MINE: -4,
+  FLAG_MINE: -5,
+  CLICKED_MINE: -6,
+  OPENED: 0, // 0 이상이 되면 다 opened
+}
+
+export const TableContext = createContext({
+  tableData: [], // 초기 값, 많은 의미는 없다.
+  dispatch: () => {},
+
+});
+
+const initialState = {
+  tableData : [],
+  timer: 0,
+  result: '',
+}
+
+export const START_GAME = 'START_GAME';
+const reducer = (state, action) => {
+  switch (action.type) {
+    case START_GAME:
+        return {
+          ...state,
+          tableData: plantMine(action.row, action.cell, action.mine),
+        }
+    default:
+        return state;
+  }
+}
+const MineSearch = () => {
+  const [ state, dispatch ] = useReducer(reducer, initialState);
+
+  const value = useMemo(() => ({ TableData: state.tableData, dispatch }), [state.tableData] );
+
+  return (
+    <TableContext.Provider value={{TableData: state.tableData, dispatch}}>
+      <Form />
+      <div>{state.timer}</div>
+      <Table />
+      <div>{state.result}</div>
+    </TableContext.Provider>
+  );
+};
+
+export default MineSearch;
